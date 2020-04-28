a20b5c3485aff1c9bc2a43b0f26d3ef08d36ac5f
lucas <lucassouzadeveloper@gmail.com>
2020-04-28 11:27:09 +0100

    finished the script, but i need to fix a small func call

--- movieSeatBooking/script.js
+++ movieSeatBooking/script.js
@@ -1,4 +1,10 @@
-//const a let from the DOM
+/**
+ * TODO: the movie localstorage when reloaded is coming wrong, needs to be fixed 
+ */  
+
+/**
+ * *const a let from the DOM
+ */
 const container = document.querySelector( '.container' );
 const seats = document.querySelectorAll( '.row .seat:not(.occupied )' );
 const count = document.getElementById( 'count' );
@@ -6,9 +12,33 @@ const total = document.getElementById( 'total' );
 const movieSelect = document.getElementById( 'movie' );
 let ticketPrice = +movieSelect.value; //changin it for number with +
 
-//updating the total and counting it 
-function updateSelectedCount () {
+
+populateUI();
+
+
+/**
+ * *Save selected movie index and price
+ */
+function setMovieData( movieIndex, moviePrice ) {
+    //sending the movie name and the price to the local storage
+    //example
+    //movie: the hole 
+    //price: &10
+    localStorage.setItem( 'selectedMovieIndex', movieIndex );
+    localStorage.setItem( 'selectedMoviePrice', moviePrice );
+}
+
+/**
+ * *updating the total and counting it 
+*/
+function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll( '.row .seat.selected' );
+
+    const seatsIndex = [ ...selectedSeats ].map( ( seat ) => [ ...seats ].indexOf( seat ) );
+
+    //sending the seats index as an array to the local storage
+    //example [0,8,5,6,15]
+    localStorage.setItem( 'selectedSeats', JSON.stringify( seatsIndex ) );
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -16,16 +46,44 @@ function updateSelectedCount () {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-//events listeners
+/**
+ * *get data from the localstorage and papulate the UI
+ */
+function populateUI() {
+    //getting back the date from the localstorage
+    const selectedSeats = JSON.parse( localStorage.getItem( 'selectedSeats' ) );
 
-//movie select event 
+    //here we loop every indice in array and give to it a class select 
+    if ( selectedSeats !== null && selectedSeats.length > 0 ) {
+        seats.forEach( ( seat, index ) => {
+            if ( selectedSeats.indexOf( index ) > -1 ) {
+                seat.classList.add( 'selected' );
+            }
+        } );
+    }
+
+    //here we get the movie name and price to populate the UI
+    const selectedMovieIndex = localStorage.getItem( 'selectedMovieIndex' );
+    if ( selectedMovieIndex !== null ) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+/** 
+* *events listeners
+* *movie select event 
+*/
 movieSelect.addEventListener( 'change', e => {
     ticketPrice = +e.target.value;
+
+    setMovieData( e.target.selectedIndex, e.target.value );
+
     updateSelectedCount();
 } );
 
-
-//seat event click
+/**
+ * *seat event click
+ */
 container.addEventListener( 'click', e => {
     if ( e.target.classList.contains( 'seat' ) && !e.target.classList.contains( 'occupied' ) ) {
         e.target.classList.toggle( 'selected' );
@@ -33,3 +91,8 @@ container.addEventListener( 'click', e => {
         updateSelectedCount();
     }
 } );
+
+/**
+ * *inicial count and total set
+ */
+updateSelectedCount();
