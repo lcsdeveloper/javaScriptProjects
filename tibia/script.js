e70da1f7f31ed6500d6da3f9ce5408da75d8fcae
unknown <lucassouzadeveloper@gmail.com>
2020-05-14 11:53:19 +0100

    added small element, input filters

--- tibia/script.js
+++ tibia/script.js
@@ -5,69 +5,89 @@ const newLevel = document.getElementById( 'new-level' );
 const newExperience = document.getElementById( 'new-experience' );
 const levelsTable = [ {
         level: 50,
-        experience: 1.847300
+        experience: 1847300
     },
     {
         level: 51,
-        experience: 1.965000
+        experience: 1965000
     },
     {
         level: 52,
-        experience: 2.087600
+        experience: 2087600
     },
     {
         level: 53,
-        experience: 2.215200
+        experience: 2215200
     },
     {
         level: 54,
-        experience: 2.347900
+        experience: 2347900
     },
     {
         level: 55,
-        experience: 2.485800
+        experience: 2485800
     },
     {
         level: 56,
-        experience: 2.629000
+        experience: 2629000
     },
     {
         level: 57,
-        experience: 2.777600
+        experience: 2777600
     },
     {
         level: 58,
-        experience: 2.931700
+        experience: 2931700
     },
     {
         level: 59,
-        experience: 3.091400
+        experience: 3091400
     },
     {
         level: 60,
-        experience: 3.256800
+        experience: 3256800
     },
 ];
 
 function cheakLevel( level, newLevel ) {
 
-    if ( level >= 50 && level <= 60 && newLevel >= 50 && newLevel <= 60 ) {
-        levelsTable.map( obj => {
-            if ( obj.level == level )
-                experience.value = obj.experience;
+    levelsTable.map( obj => {
+        if ( obj.level == level )
+            experience.value = obj.experience;
 
-            if ( obj.level == newLevel )
-                newExperience.value = obj.experience;
+        if ( obj.level == newLevel )
+            newExperience.value = obj.experience;
 
+    } );
+}
+
+function cheakInput() {
+
+    if ( level.value && newLevel.value != null ) {
+        if ( level.value >= 50 && level.value <= 60 && newLevel.value >= 50 && newLevel.value <= 60 ) {
+            cheakLevel( level.value, newLevel.value );
+            const getSmall = formExperience.querySelectorAll( 'small' );
+            getSmall.forEach( element => {
+                element.className = '';
+            } );
+        } else {
+            const getSmall = formExperience.querySelectorAll( 'small' );
+            getSmall.forEach( element => {
+                element.innerText = 'Cheack Numbers';
+                element.className = 'error';
+            } );
+        }
+    } else {
+        const getSmall = formExperience.querySelectorAll( 'small' );
+        getSmall.forEach( element => {
+            element.innerText = 'Enter a Number';
+            element.className = 'error';
         } );
     }
+
 }
 
 formExperience.addEventListener( 'submit', function ( e ) {
     e.preventDefault();
-
-    if ( level.value && newLevel.value != null )
-        cheakLevel( level.value, newLevel.value );
-    else
-        alert( "you have to insert the level and new level numbers" );
+    cheakInput( level.value.trim(), newLevel.value.trim() );
 } );
